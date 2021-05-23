28f2cf6058f926f24997a45e1231770c991d36d0
Kouki Badr <badreddinekouki1998@gmail.com>
2021-05-23 19:03:15 +0100

    initial commit

---
+++ lib/custom_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String buttonText;
+  final Color backgroundColor;
+  final Function onClickHandler;
+  final double buttonSize;
+  final double buttonHeight;
+  final Color textColor;
+
+  CustomButton(
+      {@required this.buttonText,
+      @required this.backgroundColor,
+      @required this.onClickHandler,
+      this.buttonSize = 150,
+      this.buttonHeight = 40,
+      this.textColor = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.buttonSize,
+      height: this.buttonHeight,
+      decoration: BoxDecoration(
+          color: this.backgroundColor, borderRadius: BorderRadius.circular(20)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            this.buttonText,
+            style: TextStyle(color: this.textColor),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/custom_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final Color borderColor;
+  final TextEditingController textController;
+  final double inputWidth;
+  final String placeholder;
+  final Color textColor;
+
+  CustomTextField(
+      {@required this.placeholder,
+      @required this.textController,
+      this.borderColor = Colors.black,
+      this.inputWidth = 200,
+      this.textColor = Colors.black});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.inputWidth,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: this.borderColor, width: 1)),
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: CupertinoTextField(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.transparent, width: 0)),
+          placeholder: this.placeholder,
+          controller: this.textController,
+          style: TextStyle(color: this.textColor),
+        ),
+      ),
+    );
+  }
+}
